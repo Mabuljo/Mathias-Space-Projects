f86c6b90b12b3fbb44aebe4b4b4e4a182b935e34
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2025-01-19 15:47:02 +0100

    modification de la fonction fetchAPOD

--- src/utils/FetchAPOD.js
+++ src/utils/FetchAPOD.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const FetchAPOD = () => {
     const [imgUrl, setImgUrl] = useState('');
+    const [mediaType, setMediaType] = useState('');
     const [title, setTitle] = useState('');
     const [error, setError] = useState(null);
 
@@ -12,12 +13,27 @@ const FetchAPOD = () => {
             try {
                 const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
                 );
-                setImgUrl(response.data.url);
-                setTitle(response.data.title);
+
+                const url = response.data.url;
+                const media_type = response.data.media_type;
+                const title = response.data.title;
+
+                if (media_type === 'image') {
+                    setImgUrl(url);
+                    setMediaType(media_type);
+                    setTitle(title);
+                } else {
+                    // Si le media_type n'est pas une image, afficher une image par défaut
+                    setImgUrl('/fallback.webp');
+                    setMediaType('image');
+                    setTitle('Image par défaut');
+                }
             } catch (error){
                 console.error ('Erreur lors de la récupération des données :', error);
                 setImgUrl('/fallback.webp');
+                setMediaType('image'); // Par défaut, afficher une image
                 setTitle('Image par défaut'); // Image par défaut
+                setError(error.message);
             }
         };
         fetchAPOD();
