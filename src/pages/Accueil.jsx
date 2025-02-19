be8e6af57684f0ff47dd32db5080370ac20e6974
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2025-02-19 14:56:14 +0100

    MAJ des données des astéroides

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer';
 
 const Accueil = () => {
     const pages = [
-        { title: 'rechercher', image: '/rechercher.webp', link: '/rechercher', text:`Trouvez les étoiles, les planètes et bien plus encore dans la bibliothèque d'images de la NASA !` },
+        { title: 'Rechercher', image: '/rechercher.webp', link: '/rechercher', text:`Trouvez les étoiles, les planètes et bien plus encore dans la bibliothèque d'images de la NASA !` },
         { title: 'Explorer Google Earth', image: '/earth.webp', link: 'https://earth.google.com/web', text:'Découvrez la Terre et ses merveilles grâce à Google Earth. (Une connexion peut être requise pour certaines fonctionnalités avancées.)', newTab: true },
         { title: 'Astéroïdes', image: '/asteroides.webp', link: '/asteroides', text:`L'API Asteroids - NeoWs (Near Earth Object Web Service) de la NASA permet d'obtenir des informations sur les astéroïdes proches de la Terre aujourd'hui.` },
         { title: 'Lancements à venir', image: '/fusee.webp', link: '/lancements', text:'Préparez-vous au décollage ! Consultez les dates des prochains lancements de fusées et satellites grâce aux données des SpaceDevs.' },
